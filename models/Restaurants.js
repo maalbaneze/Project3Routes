0fa7f70ea90a68a352e4df67bbf4b7e454c70d6c
Michael A <maalbaneze@aol.com>
2019-05-10 11:57:58 -0700

    zip and postal to strings only

--- models/Restaurants.js
+++ models/Restaurants.js
@@ -10,22 +10,22 @@ let RestaurantSchema = new Schema({
     },
     zip: {
         type: String,
-        validate: {
-            validator: function (v) {
-                return /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/.test(v);
-            },
-            message: props => `${props.value} is not a valid zip code`
-        },
-        required: [true, 'US zip code required']
+        // validate: {
+        //     validator: function (v) {
+        //         return /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/.test(v);
+        //     },
+        //     message: props => `${props.value} is not a valid zip code`
+        // },
+        // required: [true, 'US zip code required']
     },
     postal: {
         type: String,
-        validate: {
-            validator: function (v) {
-                return /^(?:[A-Z0-9]+([- ]?[A-Z0-9]+)*)?$/.test(v);
-            },
-            message: props => `${props.value} is not a valid postal code`
-        },
+        // validate: {
+        //     validator: function (v) {
+        //         return /^(?:[A-Z0-9]+([- ]?[A-Z0-9]+)*)?$/.test(v);
+        //     },
+        //     message: props => `${props.value} is not a valid postal code`
+        // },
         // required: [false, 'Postal code required']
     },
     // meals: [{
