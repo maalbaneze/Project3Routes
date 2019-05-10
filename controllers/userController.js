df8feca292221dcb8406183b27f775a8b47ed422
Michael A <maalbaneze@aol.com>
2019-05-10 12:51:37 -0700

    body

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,12 +13,12 @@ module.exports = {
             .catch(err => res.status(422).json(err));
     },
     create: function (req, res) {
-        db.User.create(req.query)
+        db.User.create(req.body)
             .then(dbUser => res.json(dbUser))
             .catch(err => res.status(422).json(err));
     },
     update: function (req, res) {
-        db.User.findOneAndUpdate({ _id: req.params.id }, req.query)
+        db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
             .then(dbUser => res.json(dbUser))
             .catch(err => res.status(422).json(err));
     },
