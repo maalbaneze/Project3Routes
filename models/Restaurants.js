34e7565b3be6761a8afa576b09942dd675fff27c
Michael A <maalbaneze@aol.com>
2019-05-10 13:04:03 -0700

    Restaurant unique

--- models/Restaurants.js
+++ models/Restaurants.js
@@ -7,30 +7,31 @@ const Schema = mongoose.Schema;
 let RestaurantSchema = new Schema({
     name: {
         type: String,
+        unique: true
     },
     zip: {
         type: String,
-        // validate: {
-        //     validator: function (v) {
-        //         return /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/.test(v);
-        //     },
-        //     message: props => `${props.value} is not a valid zip code`
-        // },
+        validate: {
+            validator: function (v) {
+                return /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/.test(v);
+            },
+            message: props => `${props.value} is not a valid zip code`
+        },
         // required: [true, 'US zip code required']
     },
     postal: {
         type: String,
-        // validate: {
-        //     validator: function (v) {
-        //         return /^(?:[A-Z0-9]+([- ]?[A-Z0-9]+)*)?$/.test(v);
-        //     },
-        //     message: props => `${props.value} is not a valid postal code`
-        // },
+        validate: {
+            validator: function (v) {
+                return /^(?:[A-Z0-9]+([- ]?[A-Z0-9]+)*)?$/.test(v);
+            },
+            message: props => `${props.value} is not a valid postal code`
+        },
         // required: [false, 'Postal code required']
     },
-    // meals: [{
-    //     type: Schema.Types.ObjectId, ref: 'Meal'
-    // }],
+    meals: [{
+        type: Schema.Types.ObjectId, ref: 'Meal'
+    }],
 });
 
 
